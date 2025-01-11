91859c9d0c554993feff0dc550065da27c7cd4da
Hanna Kerebińska <115366640+hkereb@users.noreply.github.com>
2025-01-11 19:54:37 +0100

    restart works - client (#35)
    * game can be restarted with the same settings
    
    * changed font size for word

--- klient/main.py
+++ klient/main.py
@@ -18,14 +18,15 @@ def handle_cant_connect(error_message):
     qtw.QMessageBox.critical(window, "Connection Error", f"Cannot connect to the server:\n{error_message}")
 
 def handle_connection_error(error_message):
-    qtw.QMessageBox.critical(window, "Connection Error",f"The connection with the server has been lost:\n{error_message}")
     window.clean_upon_disconnect()
+    qtw.QMessageBox.critical(window, "Connection Error",f"The connection with the server has been lost:\n{error_message}")
 
 def unlock_nickname():
     window.submit_nick()
 
 def disconnect_from_server():
     if client.isConnected:
+        window.clean_upon_disconnect()
         client.intentional_disconnect = True
         client.disconnect()
         qtw.QMessageBox.information(window, "Disconnected", "You have been disconnected from the server.")
@@ -52,7 +53,7 @@ if __name__ == "__main__":
     window.sig_submit_letter.connect(lambda letter: client.send_to_server("06", f"{letter}"))
     window.sig_time_ran_out.connect(lambda message: client.send_to_server("80", ""))
     window.sig_leave_room.connect(lambda message: client.send_to_server("09", ""))
-    window.sig_ready_to_play.connect(lambda message: client.send_to_server("82", ""))
+    window.sig_ready_to_play.connect(lambda message: client.send_to_server("82", message))
 
     client.sig_cant_connect.connect(lambda error_msg: handle_cant_connect(error_msg))
     client.sig_server_disconnected.connect(lambda error_msg: handle_connection_error(error_msg))
